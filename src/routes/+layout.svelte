6f6e161c01aeb3999df22c530afe94ee3799072b
unel <unel86.dev+github@proton.me>
2024-03-31 22:54:41 +0300

    some styles?..

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,84 @@
+<script>
+	import '../app.css';
+
+	const themes = [
+		{ label: 'Default', value: 'default' },
+		{ label: 'Light', value: 'light' },
+		{ label: 'Dark', value: 'dark' },
+		{ label: 'Cupcake', value: 'cupcake' },
+		{ label: 'Pastel', value: 'pastel' },
+		{ label: 'CMYK', value: 'cmyk' }
+	];
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h8m-8 6h16"
+					/></svg
+				>
+			</div>
+			<ul
+				tabindex="0"
+				class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+			>
+				<li><a href="/docs">Some "documentation"</a></li>
+				<li><a href="/models">Work with models</a></li>
+			</ul>
+		</div>
+
+		<a class="btn btn-ghost text-xl" href="/">AAI</a>
+	</div>
+
+	<div class="navbar-center hidden lg:flex">
+		<ul class="menu menu-horizontal px-1">
+			<li><a href="/docs">Some "documentation"</a></li>
+			<li><a href="/models">Work with models</a></li>
+		</ul>
+	</div>
+
+	<div class="navbar-end">
+		<div class="dropdown">
+			<div tabindex="0" role="button" class="btn m-1">
+				Theme
+				<svg
+					width="12px"
+					height="12px"
+					class="h-2 w-2 fill-current opacity-60 inline-block"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 2048 2048"
+					><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg
+				>
+			</div>
+
+			<ul tabindex="0" class="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
+				{#each themes as theme}
+					<li>
+						<input
+							type="radio"
+							name="theme-dropdown"
+							class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+							aria-label={theme.label}
+							value={theme.value}
+						/>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+</div>
+
+<main class="prose max-w-none">
+    <slot />
+</main>
--- src/routes/models/+page.svelte
+++ src/routes/models/+page.svelte
@@ -1,196 +1,203 @@
 <script lang="ts" context="module">
-	import { createApi } from "$lib/api/aai";
-	import type { Model, ModelInfo } from "$lib/api/ollama";
+	import { createApi } from '$lib/api/aai';
+	import type { Model, ModelInfo } from '$lib/api/ollama';
 
-    const api = createApi({
-        baseUrl: '',
-        fetcher: fetch,
-    });
+	const api = createApi({
+		baseUrl: '',
+		fetcher: fetch
+	});
 </script>
 
 <script lang="ts">
-    const { data } = $props();
+	const { data } = $props();
 
-    let chatText = $state('');
-    let chatResponse: string = $state('');
-    let chatMessages: Array<{ author: string, message: string }> = $state([]);
-    let abortController: AbortController | undefined = $state();
+	let chatText = $state('');
+	let chatResponse: string = $state('');
+	let chatMessages: Array<{ author: string; message: string }> = $state([]);
+	let abortController: AbortController | undefined = $state();
 
-    let models: Model[] = $state(data.models);
-    let highlightedModel: string | undefined = $state();
-    let selectedModel: string | undefined = $state();
+	let models: Model[] = $state(data.models);
+	let highlightedModel: string | undefined = $state();
+	let selectedModel: string | undefined = $state();
 
-    let modelsInfo: Record<string, ModelInfo> = $state({});
+	let modelsInfo: Record<string, ModelInfo> = $state({});
 
-    let activeModelName = $derived(highlightedModel || selectedModel);
-    let modelInfo = $derived(activeModelName ? modelsInfo[activeModelName] : undefined);
+	let activeModelName = $derived(highlightedModel || selectedModel);
+	let modelInfo = $derived(activeModelName ? modelsInfo[activeModelName] : undefined);
 
-    async function refreshModels() {
-        models = await api.getModels();
-    }
+	async function refreshModels() {
+		models = await api.getModels();
+	}
 
-    async function loadModelInfo(modelName: string) {
-        if (modelsInfo[modelName]) return;
+	async function loadModelInfo(modelName: string) {
+		if (modelsInfo[modelName]) return;
 
-        try {
-            const info = await api.getModelInfo({ modelName });
+		try {
+			const info = await api.getModelInfo({ modelName });
 
-            if (info) {
-                modelsInfo[modelName] = info;
-            }
-        } catch {
+			if (info) {
+				modelsInfo[modelName] = info;
+			}
+		} catch {}
+	}
 
-        }
-    }
+	function selectModel(modelName: string) {
+		selectedModel = modelName;
 
-    function selectModel(modelName: string) {
-        selectedModel = modelName;
+		loadModelInfo(modelName);
+	}
 
-        loadModelInfo(modelName);
-    }
+	function unselectModel() {
+		highlightedModel = undefined;
+	}
 
-    function unselectModel() {
-        highlightedModel = undefined;
-    }
+	function highlightModel(modelName: string) {
+		highlightedModel = modelName;
 
-    function highlightModel(modelName: string) {
-        highlightedModel = modelName;
+		loadModelInfo(modelName);
+	}
 
-        loadModelInfo(modelName);
-    }
+	function unhighlightModel(modelName?: string) {
+		if (modelName && modelName !== highlightedModel) return;
 
-    function unhighlightModel(modelName?: string) {
-        if (modelName && modelName !== highlightedModel) return;
+		highlightedModel = undefined;
+	}
 
-        highlightedModel = undefined;
-    }
+	async function onChatSubmit(e: SubmitEvent, modelName?: string) {
+		e.preventDefault();
+		if (!modelName) {
+			return;
+		}
+		const userMessage = String(chatText);
+		chatText = '';
 
-    async function onChatSubmit(e: SubmitEvent, modelName?: string) {
-        e.preventDefault();
-        if (!modelName) {
-            return;
-        }
-        const userMessage = String(chatText);
-        chatText = '';
+		chatMessages.push({ author: 'user', message: userMessage });
 
-        chatMessages.push({ author: 'user', message: userMessage });
+		console.log(`sending ${chatText} to ${modelName}`);
+		chatResponse = '';
+		abortController = new AbortController();
+		const signal = abortController.signal;
+		const messages = api.generateCompletion({ modelName, prompt: userMessage, signal });
 
-        console.log(`sending ${chatText} to ${modelName}`);
-        chatResponse = '';
-        abortController = new AbortController();
-        const signal = abortController.signal;
-        const messages = api.generateCompletion({ modelName, prompt: userMessage, signal });
-        
-        for await (const message of messages) {
-            console.log('response with', message);
-            chatResponse += message.text;
-        }
+		for await (const message of messages) {
+			console.log('response with', message);
+			chatResponse += message.text;
+		}
 
-        chatMessages.push({ author: 'ai', message: chatResponse });
-        chatResponse = '';
-        e.preventDefault();
-    }
+		chatMessages.push({ author: 'ai', message: chatResponse });
+		chatResponse = '';
+		e.preventDefault();
+	}
 
-    function abortChatResponse() {
-        if (!abortController) return;
-        abortController.abort();
+	function abortChatResponse() {
+		if (!abortController) return;
+		abortController.abort();
 
-        chatMessages.push({ author: 'ai', message: chatResponse });
-        chatResponse = '';
-        abortController = undefined;
-    }
+		chatMessages.push({ author: 'ai', message: chatResponse });
+		chatResponse = '';
+		abortController = undefined;
+	}
 </script>
-
-<style lan>
-    .models-block {
-        display: flex;
-        flex-direction: row;
-        column-gap: 16px;
-    }
-
-    .models-list {
-        display: block;
-        padding: 8px;
-        border: 1px solid silver
-    }
-
-    .models-list .model-item {
-        cursor: pointer;
-    }
-
-    .models-list .model-item:hover,
-    .models-list .model-item_ac {
-        background-color: cornflowerblue;
-    }
-
-    .model-info {
-        flex-grow: 1;
-        display: block;
-        padding: 8px;
-    }
-</style>
 
 <h1>Models list</h1>
 
 <section class="models-block">
-    <ul class="models-list">
-        {#each models as model}
-            <li 
-                class={`model-item${model.name === activeModelName ? ' model-item_ac' : ''}`} 
-                onclick={() => selectModel(model.name)}
-                onmouseenter={() => highlightModel(model.name)}
-                onmouseleave={() => unhighlightModel(model.name)}
-            >
-                <b>{model.name}</b>
-            </li>
-        {/each}
-    </ul>
+	<ul class="models-list">
+		{#each models as model}
+			<li
+				class={`model-item${model.name === activeModelName ? ' model-item_ac' : ''}`}
+				onclick={() => selectModel(model.name)}
+				onmouseenter={() => highlightModel(model.name)}
+				onmouseleave={() => unhighlightModel(model.name)}
+			>
+				<b>{model.name}</b>
+			</li>
+		{/each}
+	</ul>
 
-    <section class="model-info">
-        {#if modelInfo}
-            <table style={`width: 100%;`}>
-                <tbody>
-                    <tr><td>template</td><td>{modelInfo.template}</td></tr>
-                    <tr><td>parameters</td><td> {modelInfo.parameters}</td></tr>
-                </tbody>
-            </table>
-            <pre>{JSON.stringify(modelInfo.details, undefined, 4)}</pre>
+	<section class="model-info">
+		{#if modelInfo}
+			<table class="table" style={`width: 100%;`}>
+				<tbody>
+					<tr><td>template</td><td>{modelInfo.template}</td></tr>
+					<tr><td>parameters</td><td> {modelInfo.parameters}</td></tr>
+				</tbody>
+			</table>
+			<pre>{JSON.stringify(modelInfo.details, undefined, 4)}</pre>
+		{/if}
 
-            <hr />
-        {/if}
+		{#if activeModelName}
+			<form class="chact-section" onsubmit={(e) => onChatSubmit(e, activeModelName)}>
+				<div class="chat-messages w-full">
+					{#each chatMessages as message}
+						<div class={`chat ${message.author === 'ai' ? 'chat-end' : 'chat-start'}`}>
+							<div class="chat-header">
+								{message.author}:
+							</div>
 
-        {#if activeModelName}
-            <form class="chat" onsubmit={(e) => onChatSubmit(e, activeModelName)}>
-                {#each chatMessages as message}
-                    <div>
-                        <span>
-                            {message.author}:
-                        </span>
-                        <span>
-                            {message.message}
-                        </span>
-                    </div>
-                {/each}
+							<div class="chat-bubble">
+								{message.message}
+							</div>
+						</div>
+					{/each}
 
-                {#if chatResponse}
-                    <div>
-                        <span>
-                            ai:
-                            {#if abortController}
-                            <button type="button" onclick={abortChatResponse}>[abort]</button>
-                            {/if}
-                        </span>
-                        <span>
-                            {chatResponse}
-                        </span>
-                    </div>
-                {/if}
+					{#if chatResponse}
+						<div class="chat chat-end">
+							<div class="chat-header">
+								ai:
+							</div>
 
-                <input type="text" bind:value={chatText}>
-                <button type="submit" disabled={!!chatResponse}>{`>>`}</button>
-            </form>
-        {/if}
-    </section>
+							<div class="chat-bubble">
+								{chatResponse}
+							</div>
+
+                            <div class="chat-footer">
+                                <button class="btn btn-xs btn-link" type="button" onclick={abortChatResponse}>abort</button>
+                            </div>
+						</div>
+					{/if}
+				</div>
+
+                <div class="chat-footer flex justify-items-stretch gap-x-4">
+                    <input type="text" class="input input-bordered grow" bind:value={chatText} />
+                    <button class="btn" type="submit" disabled={!!chatResponse}>{`>>`}</button>
+                </div>
+			</form>
+		{/if}
+	</section>
 </section>
 
-<button onclick={refreshModels} >refresh models</button>
+<button class="btn btn-sm" onclick={refreshModels}>refresh models</button>
+
+<style lan>
+	.models-block {
+		display: flex;
+		flex-direction: row;
+		column-gap: 16px;
+	}
+
+	.models-list {
+		display: block;
+		padding: 8px;
+		border: 1px solid silver;
+	}
+
+	.models-list .model-item {
+		cursor: pointer;
+	}
+
+	.models-list .model-item:hover,
+	.models-list .model-item_ac {
+		background-color: cornflowerblue;
+	}
+
+	.model-info {
+		flex-grow: 1;
+		display: block;
+		padding: 8px;
+	}
+
+	.chat-section {
+		display: block;
+	}
+</style>
