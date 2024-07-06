dcb03cd079c2bc3bbcc65255d293ac7d00201590
unel <unel86.dev+github@proton.me>
2024-07-06 19:47:39 +0300

    meow

--- src/lib/api/lmstudio/index.ts
+++ src/lib/api/lmstudio/index.ts
@@ -68,6 +68,7 @@ export function createApi({ baseUrl, fetcher }: CreateApiParams) {
             const url = `${baseUrl}/v1/chat/completions`;
             const fetchOptions = {
                 headers: {
+                    "Accept": "application/json",
                     "Content-Type": "application/json",
                 },
                 method: 'POST',
@@ -76,21 +77,32 @@ export function createApi({ baseUrl, fetcher }: CreateApiParams) {
             };
             console.log('lmstudio generate completion', body);
 
-            // console.log(`fetch("${url}", ${JSON.stringify(fetchOptions)})`);
+            console.log(`fetch("${url}", ${JSON.stringify(fetchOptions)})`);
             const response = await fetcher(url, fetchOptions);
+            console.log('response', response);
 
-            for await (const chunk of response.body) {
-                const data = JSON.parse(decoder.decode(chunk));
-                console.log('rd', data);
-                if (signal?.aborted) {
-                    return;
-                }
-                
-                yield {
-                    text: data.response,
-                    done: data.done,
-                };
-            } 
+            try {
+                for await (const chunk of response.body) {
+                    console.log('response chunk::', chunk);
+                    const str = decoder.decode(chunk);
+                    const jsonStr = str.slice(6);
+                    console.log('response string::', str, jsonStr);
+                    const data = JSON.parse(jsonStr);
+                    console.log('rd', data);
+                    if (signal?.aborted) {
+                        return;
+                    }
+
+                    console.log('text', data.choices[0].delta.content);
+                    
+                    yield {
+                        text: data.choices[0].delta.content,
+                        done: !!data.finish_reason,
+                    };
+                } 
+            } catch (e) {
+                console.log('error', e);
+            }
         },
 
         async pullModel({ modelName }: PullModelParams) {
